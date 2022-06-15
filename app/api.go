db028090ee26df028809589fcf3d453fd6f20933
Marios <30930688+mariospapaz@users.noreply.github.com>
2022-06-15 21:53:32 +0300

    First steps

--- app/api.go
+++ app/api.go
@@ -1,5 +1,54 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func GetAllWorkouts(ctx *gin.Context) {
+
+}
+
+func GetDayWorkout(ctx *gin.Context) {
+
+}
+
+func RouterPaths(engine *gin.Engine) {
+	engine.GET("/api/workout", GetAllWorkouts)
+	engine.GET("/api/workout/:day", GetDayWorkout)
+}
+
+func ConnectDB() *mongo.Collection {
+	// Must use Mongo 5.0 stable
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:example@bibi:27017/my-workout?authSource=admin"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
+	log.Println("Connected to MongoDB!")
+	logCollection := client.Database("my-workout").Collection("temp")
+	return logCollection
+}
+
 func main() {
 
+	log.Println("##############################################")
+	log.Println("###       Connecting to Database        ######")
+	log.Println("##############################################")
+
+	col := ConnectDB()
+
+	log.Println(col)
+
+	router := gin.Default()
+	RouterPaths(router)
 }
