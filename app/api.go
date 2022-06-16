1550517ed6207b61010c1fc743474d03d8e969c1
Marios <30930688+mariospapaz@users.noreply.github.com>
2022-06-16 11:51:42 +0300

    fixed mongodb not inserting data

--- app/api.go
+++ app/api.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -24,32 +29,78 @@ func RouterPaths(engine *gin.Engine) {
 	engine.Run("localhost:8080")
 }
 
-func ConnectDB() *mongo.Collection {
-	// Must use Mongo 5.0 stable
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://root:example@bibi:27017/"))
+func PrintLog(line string) {
+	log.Println("##############################################")
+	log.Printf("###       %s                           ######\n", line)
+	log.Println("##############################################")
+}
+
+func ConnectDB() {
+
+	// Must use Mongo 5.0 stable (note: change localhost to 'bibi' when the image is about to upload)
+
+	PrintLog("Connecting to Database")
+
+	const uri = "mongodb://root:example@localhost:27017/?maxPoolSize=20&w=majority"
+	const data_path = "../.mongo/data.json"
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
 
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
 		panic(err)
 	}
 
-	log.Println("Connected to MongoDB!")
-	logCollection := client.Database("my-workout").Collection("temp")
-	return logCollection
+	PrintLog("Connected to MongoDB")
+
+	logCollection := client.Database("database").Collection("temp")
+
+	databases, err := client.ListDatabases(ctx, bson.M{})
+	if err != nil {
+		panic(err)
+	}
+
+	file, err := os.Open(data_path)
+	if err != nil {
+		panic(err)
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+
+	var Plan []interface{}
+
+	if err := json.Unmarshal(bytes, &Plan); err != nil {
+		panic(err)
+	}
+
+	log.Println(databases)
+
+	logCollection.InsertMany(ctx, Plan)
+
+	PrintLog("Data inserted")
+
+	file.Close()
 }
 
 func main() {
 
-	log.Println("##############################################")
-	log.Println("###       Connecting to Database        ######")
-	log.Println("##############################################")
+	ConnectDB()
 
-	col := ConnectDB()
-
-	log.Println(col)
-
-	router := gin.Default()
-	RouterPaths(router)
+	//router := gin.Default()
+	//RouterPaths(router)
 }
